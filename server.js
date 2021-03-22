f48219a02c2cbd8acf3bc802721888b8cbb9ad0e
AymKh <aymen4real@gmail.com>
2021-03-22 17:03:36 +0100

    updated port to use process.env.PORT

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 3000 || process.env.PORT;
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
 var firebase = require('firebase');
